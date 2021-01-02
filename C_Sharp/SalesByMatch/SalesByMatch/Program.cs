0e8f5e60567efa91c0b26f6bfa6a7293b10d6645
DeShawn D <deshawn.dyson.career@gmail.com>
2021-01-02 18:23:19 -0500

    This is the solution to the HackerRank Challenge: Sales By Match

---
+++ C_Sharp/SalesByMatch/SalesByMatch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesByMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            int[] testing = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+
+            Console.WriteLine(sockMerchant(9, testing));
+        }
+
+        // Complete the sockMerchant function below.
+        static int sockMerchant(int n, int[] ar)
+        {
+            int totalNumOfSockPairs = 0;
+            Dictionary<int, int> sockCollection = new Dictionary<int, int>();
+
+            foreach (int sock in ar)
+            {
+                if(sockCollection.ContainsKey(sock))
+                {
+                    sockCollection[sock]++;
+                }
+                else
+                {
+                    sockCollection.Add(sock, 1);
+                } 
+            }
+
+            foreach(KeyValuePair<int, int> sockCount in sockCollection)
+            {
+                if(sockCount.Value > 1)
+                {
+                    totalNumOfSockPairs += sockCount.Value / 2;
+                }
+            }
+
+
+            return totalNumOfSockPairs;
+        }
+    }
+}
